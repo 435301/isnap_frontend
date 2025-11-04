0a04b5f7356055c2c5eabfbf1a5d468157def5f6
RICH LABZ <padmavathi.richlabz@gmail.com>
2025-11-04 16:10:02 +0530

    config

--- src/config/config.jsx
+++ src/config/config.jsx
@@ -1,4 +1,4 @@
 // config/config.js
 // const BASE_URL = "https://isnapbackend.duckdns.org"; 
-const BASE_URL = "http://192.168.29.251:8080"; 
+const BASE_URL = "https://43.204.192.110"; 
 export default BASE_URL;
